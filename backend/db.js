0daf24f210d2ddd911a5a059148630ebe4515bc3
Moch Mutaqin <mochmutaqin@gmail.com>
2026-06-23 20:26:08 +0700

    fix: use explicit host console

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 
 dns.setDefaultResultOrder('ipv4first');
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
