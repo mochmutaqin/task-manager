e27f114b1583afa81f4c0741236afa6e40613e3c
Moch Mutaqin <mochmutaqin@gmail.com>
2026-06-24 09:32:32 +0700

    fix: allow all cors origins

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // Test koneksi database
